9c7ffbb41b7d3497afbeb2c2e2223350bb6677c5
Josh Long <josh@joshlong.com>
2020-08-06 02:24:00 -0700

    settings be gone

--- integration/src/main/java/rsb/rsocket/integration/integration/IntegrationApplication.java
+++ integration/src/main/java/rsb/rsocket/integration/integration/IntegrationApplication.java
@@ -3,24 +3,50 @@ package rsb.rsocket.integration.integration;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.rsocket.messaging.RSocketStrategiesCustomizer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.codec.json.Jackson2JsonDecoder;
+import org.springframework.http.codec.json.Jackson2JsonEncoder;
 import org.springframework.integration.dsl.IntegrationFlow;
 import org.springframework.integration.dsl.IntegrationFlows;
 import org.springframework.integration.handler.GenericHandler;
+import org.springframework.integration.rsocket.ClientRSocketConnector;
 import org.springframework.integration.rsocket.RSocketInteractionModel;
 import org.springframework.integration.rsocket.dsl.RSockets;
 import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.rsocket.RSocketStrategies;
+import rsb.rsocket.BootifulProperties;
 import rsb.rsocket.integration.GreetingResponse;
 
 @Log4j2
 @SpringBootApplication
 public class IntegrationApplication {
 
-	@Bean
+    @Bean
+    RSocketStrategiesCustomizer customizer() {
+        return strategies -> strategies//
+                .encoder(new Jackson2JsonEncoder())//
+                .decoder(new Jackson2JsonDecoder())//
+                .build();
+    }
+
+    @Bean
+    ClientRSocketConnector clientRSocketConnector(
+            RSocketStrategies strategies,
+            BootifulProperties properties) {
+        ClientRSocketConnector clientRSocketConnector =
+                new ClientRSocketConnector(properties.getRsocket().getHostname(), properties.getRsocket().getPort());
+        clientRSocketConnector.setRSocketStrategies(strategies);
+//        clientRSocketConnector.setSetupRoute("clientConnect/{user}");
+//        clientRSocketConnector.setSetupRouteVariables("myUser");
+        return clientRSocketConnector;
+    }
+
+
+    @Bean
     IntegrationFlow rsocketFlow() {
         var gateway = RSockets//
                 .inboundGateway("greetings")//
-                .
                 .interactionModels(RSocketInteractionModel.requestStream);
         return IntegrationFlows
                 .from(gateway)
@@ -35,8 +61,8 @@ public class IntegrationApplication {
                 .get();
     }
 
-	public static void main(String[] a) {
-		SpringApplication.run(IntegrationApplication.class, a);
-	}
+    public static void main(String[] a) {
+        SpringApplication.run(IntegrationApplication.class, a);
+    }
 
 }
