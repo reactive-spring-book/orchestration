9e89d9c7dfcd4fd9c24c3e4f20ae5b64208cb252
Josh Long <josh@joshlong.com>
2020-08-06 05:02:54 -0700

    polish

--- integration/src/main/java/rsb/rsocket/integration/integration/IntegrationApplication.java
+++ integration/src/main/java/rsb/rsocket/integration/integration/IntegrationApplication.java
@@ -27,67 +27,75 @@ import rsb.rsocket.integration.GreetingResponse;
 @SpringBootApplication
 public class IntegrationApplication {
 
-    /*FluxMessageChannel*/
+	/* FluxMessageChannel */
 
-    @Bean
-    MessageChannel channel() {
-        return MessageChannels.flux().get();
-    }
+	@Bean
+	MessageChannel channel() {
+		return MessageChannels.flux().get();
+	}
 
-    @Bean
-    ClientRSocketConnector clientRSocketConnector(
-            RSocketStrategies strategies,
-            BootifulProperties properties) {
-        ClientRSocketConnector clientRSocketConnector = new ClientRSocketConnector(properties.getRsocket().getHostname(), properties.getRsocket().getPort());
-        clientRSocketConnector.setRSocketStrategies(strategies);
-        return clientRSocketConnector;
-    }
+	@Bean
+	ClientRSocketConnector clientRSocketConnector(RSocketStrategies strategies,
+			BootifulProperties properties) {
+		ClientRSocketConnector clientRSocketConnector = new ClientRSocketConnector(
+				properties.getRsocket().getHostname(), properties.getRsocket().getPort());
+		clientRSocketConnector.setRSocketStrategies(strategies);
+		return clientRSocketConnector;
+	}
 
-    @Bean
-    IntegrationFlow greetingFlow(ClientRSocketConnector clientRSocketConnector) {
-        var names = new String[]{"Mario", "Richard", "Michelle", "Natalie", "Madhura", "Violetta", "Yuxin", "Olga", "Rob", "Jane", "Arun", "Heinz", "Venkat"};
-        var messageSource = new MessageSource<String>() {
+	@Bean
+	IntegrationFlow greetingFlow(ClientRSocketConnector clientRSocketConnector) {
+		var names = new String[] { "Mario", "Richard", "Michelle", "Natalie", "Madhura",
+				"Violetta", "Yuxin", "Olga", "Rob", "Jane", "Arun", "Heinz", "Venkat" };
+		var messageSource = new MessageSource<String>() {
 
-            @Override
-            public Message<String> receive() {
-                var len = names.length;
-                var rand = Math.random() * len;
-                var index = (int) rand;
-                Assert.state(index <= (names.length - 1), "the index is not within range");
-                var name = names[index];
-                return MessageBuilder.withPayload(name).build();
-            }
-        };
-        return IntegrationFlows//
-                .from(messageSource, poller -> poller.poller(pm -> pm.fixedRate(1000)))//
-                .transform(String.class, GreetingRequest::new)//
-                .handle(RSockets//
-                        .outboundGateway("greetings")//
-                        .interactionModel(RSocketInteractionModel.requestStream)//
-                        .expectedResponseType(GreetingResponse.class)//
-                        .clientRSocketConnector(clientRSocketConnector)//
-                )
-                .channel(this.channel())
-                .get();
-    }
+			@Override
+			public Message<String> receive() {
+				var len = names.length;
+				var rand = Math.random() * len;
+				var index = (int) rand;
+				Assert.state(index <= (names.length - 1),
+						"the index is not within range");
+				var name = names[index];
+				return MessageBuilder.withPayload(name).build();
+			}
+		};
+		return IntegrationFlows//
+				.from(messageSource, poller -> poller.poller(pm -> pm.fixedRate(1000)))//
+				.transform(String.class, GreetingRequest::new)//
+				.handle(RSockets//
+						.outboundGateway("greetings")//
+						.interactionModel(RSocketInteractionModel.requestStream)//
+						.expectedResponseType(GreetingResponse.class)//
+						.clientRSocketConnector(clientRSocketConnector)//
+				).channel(this.channel()).get();
+	}
 
-    @Bean
-    IntegrationFlow greetingsResponseFlow() {
-        return IntegrationFlows
-                .from(this.channel())
-                .fluxTransform(messageFlux -> messageFlux.map(Message::getPayload))
-                .handle(new GenericHandler<Flux<GreetingResponse>>() {
-                    @Override
-                    public Object handle(Flux<GreetingResponse> responses, MessageHeaders headers) {
-                        responses.subscribe(gr -> log.info(gr.toString()));//todo there has to be someway to flatten this and handle the messages downstream using SI handlers?
-                        return null;
-                    }
-                })
-                .get();
-    }
+	@Bean
+	IntegrationFlow greetingsResponseFlow() {
+		return IntegrationFlows.from(this.channel())
+				.fluxTransform(messageFlux -> messageFlux.map(Message::getPayload))
+				.handle(new GenericHandler<Flux<GreetingResponse>>() {
+					@Override
+					public Object handle(Flux<GreetingResponse> responses,
+							MessageHeaders headers) {
+						responses.subscribe(gr -> log.info(gr.toString()));// todo there
+																			// has to be
+																			// someway to
+																			// flatten
+																			// this and
+																			// handle the
+																			// messages
+																			// downstream
+																			// using SI
+																			// handlers?
+						return null;
+					}
+				}).get();
+	}
 
-    public static void main(String[] a) {
-        SpringApplication.run(IntegrationApplication.class, a);
-    }
+	public static void main(String[] a) {
+		SpringApplication.run(IntegrationApplication.class, a);
+	}
 
 }
