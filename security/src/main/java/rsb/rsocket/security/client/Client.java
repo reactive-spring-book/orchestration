3e112db21ce9b2b5d7889aa6887a22e7e55af7a8
Josh Long <josh@joshlong.com>
2020-08-05 23:40:23 -0700

    polish

---
+++ security/src/main/java/rsb/rsocket/security/client/Client.java
@@ -0,0 +1,53 @@
+package rsb.rsocket.security.client;
+
+import io.rsocket.metadata.WellKnownMimeType;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.boot.rsocket.messaging.RSocketStrategiesCustomizer;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
+import org.springframework.messaging.rsocket.RSocketRequester;
+import org.springframework.security.rsocket.metadata.SimpleAuthenticationEncoder;
+import org.springframework.security.rsocket.metadata.UsernamePasswordMetadata;
+import org.springframework.stereotype.Component;
+import org.springframework.util.MimeType;
+import org.springframework.util.MimeTypeUtils;
+import reactor.core.publisher.Mono;
+import rsb.rsocket.BootifulProperties;
+import rsb.rsocket.security.GreetingResponse;
+
+@Log4j2
+@Component
+class Client {
+
+	private final MimeType mimeType = MimeTypeUtils
+			.parseMimeType(WellKnownMimeType.MESSAGE_RSOCKET_AUTHENTICATION.getString());
+
+	private final UsernamePasswordMetadata credentials = new UsernamePasswordMetadata(
+			"jlong", "pw");
+
+	@Bean
+	RSocketStrategiesCustomizer rSocketStrategiesCustomizer() {
+		return strategies -> strategies.encoder(new SimpleAuthenticationEncoder());
+	}
+
+	@Bean
+	RSocketRequester rSocketRequester(BootifulProperties properties,
+			RSocketRequester.Builder builder) {
+		return builder//
+				.setupMetadata(this.credentials, this.mimeType) // OPTIONAL!
+				.connectTcp(properties.getRsocket().getHostname(),
+						properties.getRsocket().getPort())
+				.block();
+	}
+
+	@Bean
+	ApplicationListener<ApplicationReadyEvent> ready(RSocketRequester greetings) {
+		return args -> greetings.route("greetings")//
+				.metadata(this.credentials, this.mimeType)//
+				.data(Mono.empty())//
+				.retrieveFlux(GreetingResponse.class)//
+				.subscribe(gr -> log.info("secured response: " + gr.toString()));
+	}
+
+}
--- security/src/main/java/rsb/rsocket/security/service/SecurityApplication.java
+++ security/src/main/java/rsb/rsocket/security/service/SecurityApplication.java
@@ -2,27 +2,6 @@ package rsb.rsocket.security.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.rsocket.RSocketStrategies;
-import org.springframework.messaging.rsocket.annotation.support.RSocketMessageHandler;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.rsocket.RSocketSecurity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.rsocket.core.PayloadSocketAcceptorInterceptor;
-import org.springframework.security.messaging.handler.invocation.reactive.AuthenticationPrincipalArgumentResolver;
-import org.springframework.stereotype.Controller;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
-import rsb.rsocket.security.GreetingRequest;
-import rsb.rsocket.security.GreetingResponse;
-
-import java.time.Duration;
-import java.util.stream.Stream;
 
 @SpringBootApplication
 public class SecurityApplication {
@@ -30,52 +9,6 @@ public class SecurityApplication {
 	public static void main(String args[]) {
 		System.setProperty("spring.profiles.active", "service");
 		SpringApplication.run(SecurityApplication.class, args);
-	}
-
-}
-
-@Configuration
-class SecurityConfiguration {
-
-	@Bean
-	RSocketMessageHandler rSocketMessageHandler(RSocketStrategies strategies) {
-		var mh = new RSocketMessageHandler();
-		mh.getArgumentResolverConfigurer()
-				.addCustomResolver(new AuthenticationPrincipalArgumentResolver());
-		mh.setRSocketStrategies(strategies);
-		return mh;
-	}
-
-	@Bean
-	MapReactiveUserDetailsService authentication() {
-		return new MapReactiveUserDetailsService(
-				User.withDefaultPasswordEncoder().username("rwinch").password("pw")
-						.roles("ADMIN", "USER").build(),
-				User.withDefaultPasswordEncoder().username("jlong").password("pw")
-						.roles("USER").build());
-	}
-
-	@Bean
-	PayloadSocketAcceptorInterceptor authorization(RSocketSecurity security) {
-		return security.simpleAuthentication(Customizer.withDefaults()).build();
-	}
-
-}
-
-@Controller
-class GreetingsController {
-
-	@MessageMapping("greetings")
-	Flux<GreetingResponse> greet(@AuthenticationPrincipal Mono<UserDetails> user) {
-		return user.map(UserDetails::getUsername).map(GreetingRequest::new)
-				.flatMapMany(this::greet);
-	}
-
-	private Flux<GreetingResponse> greet(GreetingRequest request) {
-		return Flux
-				.fromStream(Stream.generate(
-						() -> new GreetingResponse("Hello, " + request.getName() + "!")))
-				.delayElements(Duration.ofSeconds(1));
 	}
 
 }
