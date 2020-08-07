269282b5b43f63c2890b7891bd1336a22d1b9c74
Josh Long <josh@joshlong.com>
2020-08-06 23:20:29 -0700

    polish

--- integration/src/main/java/rsb/rsocket/integration/integration/IntegrationApplication.java
+++ integration/src/main/java/rsb/rsocket/integration/integration/IntegrationApplication.java
@@ -1,25 +1,25 @@
 package rsb.rsocket.integration.integration;
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.integration.core.MessageSource;
 import org.springframework.integration.dsl.IntegrationFlow;
 import org.springframework.integration.dsl.IntegrationFlows;
 import org.springframework.integration.dsl.MessageChannels;
+import org.springframework.integration.file.dsl.Files;
 import org.springframework.integration.handler.GenericHandler;
 import org.springframework.integration.rsocket.ClientRSocketConnector;
 import org.springframework.integration.rsocket.RSocketInteractionModel;
 import org.springframework.integration.rsocket.dsl.RSockets;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.rsocket.RSocketStrategies;
-import org.springframework.messaging.support.MessageBuilder;
 import rsb.rsocket.BootifulProperties;
 import rsb.rsocket.integration.GreetingRequest;
 import rsb.rsocket.integration.GreetingResponse;
 
-import java.util.concurrent.atomic.AtomicInteger;
+import java.io.File;
 
 @Log4j2
 @SpringBootApplication
@@ -40,23 +40,12 @@ public class IntegrationApplication {
 	}
 
 	@Bean
-	MessageSource<String> arrayMessageSource() {
-		var names = new String[] { "Mia", "Michelle", "Mario", "Richard", "Tammie",
-				"Kimly", "Natalie", "Madhura", "Violetta", "Zhen", "Justin", "Yuxin",
-				"Olga", "Rob", "Jane", "Artem", "Gary", "Mark", "Oleg", "Arun", "Heinz",
-				"Venkat" };
-		var ctr = new AtomicInteger();
-		return () -> {
-			var indx = ctr.getAndIncrement();
-			return (indx < names.length) ? MessageBuilder.withPayload(names[indx]).build()
-					: null;
-		};
-	}
-
-	@Bean
-	IntegrationFlow greetingFlow(ClientRSocketConnector clientRSocketConnector) {
+	IntegrationFlow greetingFlow(@Value("${user.home}") File home,
+			ClientRSocketConnector clientRSocketConnector) {
+		var inboundFileAdapter = Files.inboundAdapter(new File(home, "in"))
+				.autoCreateDirectory(true);
 		return IntegrationFlows//
-				.from(arrayMessageSource(),
+				.from(inboundFileAdapter,
 						poller -> poller.poller(pm -> pm.fixedRate(100)))//
 				.transform(String.class, GreetingRequest::new)//
 				.handle(RSockets//
