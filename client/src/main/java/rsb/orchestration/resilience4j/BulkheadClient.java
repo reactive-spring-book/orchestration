4b8faec2d157d0aa1f009f85694c196feb746ffb
Josh Long <josh@joshlong.com>
2020-08-25 03:16:45 -0700

    polish

--- client/src/main/java/rsb/orchestration/resilience4j/BulkheadClient.java
+++ client/src/main/java/rsb/orchestration/resilience4j/BulkheadClient.java
@@ -19,7 +19,7 @@ import java.util.UUID;
 
 @Log4j2
 @Component
-// @Profile("bulkhead")
+@Profile("bulkhead")
 @RequiredArgsConstructor
 class BulkheadClient implements ApplicationListener<ApplicationReadyEvent> {
 
