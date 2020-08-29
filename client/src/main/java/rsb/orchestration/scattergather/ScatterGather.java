dfa32215349fba0ac37b4f55d3bb8fe359f298d3
Josh Long <josh@joshlong.com>
2020-08-29 15:44:14 -0700

    polish

---
+++ client/src/main/java/rsb/orchestration/scattergather/ScatterGather.java
@@ -0,0 +1,59 @@
+package rsb.orchestration.scattergather;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import rsb.orchestration.Customer;
+import rsb.orchestration.Order;
+import rsb.orchestration.Profile;
+import rsb.orchestration.TimerUtils;
+
+import java.sql.Time;
+
+import static rsb.orchestration.TimerUtils.cache;
+
+@Log4j2
+@RequiredArgsConstructor
+@Component
+class ScatterGather implements ApplicationListener<ApplicationReadyEvent> {
+
+	private final CrmClient client;
+
+	@Override
+	public void onApplicationEvent(ApplicationReadyEvent event) {
+		var ids = new Integer[] { 1, 2, 7, 5 };
+		Flux<Customer> customerFlux = TimerUtils.cache(client.getCustomers(ids));
+		Flux<Order> ordersFlux = TimerUtils.cache(client.getOrders(ids));
+		Flux<CustomerOrders> customerOrdersFlux = customerFlux//
+				.flatMap(customer -> {
+					Flux<Order> filteredOrdersFlux = ordersFlux
+							.filter(o -> o.getCustomerId().equals(customer.getId()));
+					Mono<Profile> profileMono = client.getProfile(customer.getId());
+					Mono<Customer> customerMono = Mono.just(customer);
+					return Flux.zip(customerMono, filteredOrdersFlux.collectList(),
+							profileMono);
+				})//
+				.map(tuple -> new CustomerOrders(tuple.getT1(), tuple.getT2(),
+						tuple.getT3()));
+
+		for (var i = 0; i < 5; i++) // it gets faster after successive runs
+			run(customerOrdersFlux);
+	}
+
+	private void run(Flux<CustomerOrders> customerOrdersFlux) {
+		TimerUtils //
+				.monitor(customerOrdersFlux)//
+				.subscribe(customerOrder -> {
+					log.info("---------------");
+					log.info(customerOrder.getCustomer());
+					log.info(customerOrder.getProfile());
+					customerOrder.getOrders().forEach(order -> log
+							.info(customerOrder.getCustomer().getId() + ": " + order));
+				});
+	}
+
+}
